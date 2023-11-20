9364729c6b79c12044183d986fd0a7f54adb813f
sebastian-nunez <02snunez@gmail.com>
2023-11-20 08:08:09 -0500

    added the /process-video for GC Pub/Sub to handle messages processing videos from queue

--- video-processing-service/src/controllers/process-video.ts
+++ video-processing-service/src/controllers/process-video.ts
@@ -1,34 +1,75 @@
 import { Request, Response } from "express";
-import { DEFAULT_VIDEO_RESOLUTION } from "../utils/constants";
-import { convertVideoWithFFmpeg } from "../utils/helpers";
+import {
+  convertVideo,
+  deleteProcessedVideo,
+  deleteRawVideo,
+  downloadRawVideo,
+  uploadProcessedVideo
+} from "./video-storage";
 
 /**
- * @description Processes a video from the {@link inputFilePath} and saves it to the {@link outputFilePath}.
+ * Process a video file via Google Cloud Pub/Sub. This endpoint is triggered by a Google Cloud Pub/Sub message.
+ *
+ * Docs: https://cloud.google.com/run/docs/tutorials/pubsub#run_pubsub_server-nodejs
  */
 export const processVideo = async (req: Request, res: Response) => {
-  const { inputFilePath, outputFilePath } = req.body;
+  // get the bucket and filename from Google Cloud Pub/Sub message
+  const pubSubMessage = req.body?.message;
 
-  if (!inputFilePath || !outputFilePath) {
-    return res.status(400).json({
-      message: "ERROR: Please provide a valid inputFilePath and outputFilePath"
-    });
+  // validate the message
+  if (!pubSubMessage || !pubSubMessage?.data) {
+    return res
+      .status(400)
+      .json({ message: `Bad Request: missing Pub/Sub message or data` });
   }
+
+  let data;
 
   try {
-    await convertVideoWithFFmpeg(
-      inputFilePath,
-      outputFilePath,
-      DEFAULT_VIDEO_RESOLUTION
-    );
+    const message = Buffer.from(pubSubMessage?.data, "base64").toString("utf8");
+    data = JSON.parse(message);
 
-    return res.status(200).json({
-      message: `Video processed successfully: ${outputFilePath}`
-    });
+    if (!data?.name) {
+      throw new Error("Bad Request: Invalid message payload received");
+    }
   } catch (err) {
-    console.error(err);
+    console.log(err);
 
-    return res.status(500).json({
-      message: `ERROR: ${(err as Error).message}`
-    });
+    return res.status(400).json({ message: `Bad Request: missing filename` });
   }
+
+  // get the filename from the message
+  const inputFileName = data.name;
+  const outputFileName = `processed-${inputFileName}`;
+
+  // download video
+  await downloadRawVideo(inputFileName);
+
+  // convert video
+  try {
+    await convertVideo(inputFileName, outputFileName);
+  } catch (err) {
+    console.log(`Error converting video at ${inputFileName}: ${err}`);
+
+    // cleanup
+    await Promise.all([
+      deleteRawVideo(inputFileName),
+      deleteProcessedVideo(outputFileName)
+    ]);
+
+    return res
+      .status(500)
+      .json({ message: `Internal Server Error: video processing failed` });
+  }
+
+  // upload video
+  await uploadProcessedVideo(outputFileName);
+
+  // cleanup
+  await Promise.all([
+    deleteRawVideo(inputFileName),
+    deleteProcessedVideo(outputFileName)
+  ]);
+
+  return res.status(200).json({ message: `Video processed successfully` });
 };
