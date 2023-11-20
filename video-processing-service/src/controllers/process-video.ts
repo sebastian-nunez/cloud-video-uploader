d2bc0f7b7b887dd5d431fb5ef8b5507eb5662477
sebastian-nunez <02snunez@gmail.com>
2023-11-20 07:24:11 -0500

    Refactor video processing controller to use FFmpeg video converter helper

--- video-processing-service/src/controllers/process-video.ts
+++ video-processing-service/src/controllers/process-video.ts
@@ -1,32 +1,34 @@
 import { Request, Response } from "express";
-import { convertVideo } from "./storage";
+import { DEFAULT_VIDEO_RESOLUTION } from "../utils/constants";
+import { convertVideoWithFFmpeg } from "../utils/helpers";
 
 /**
- * @description Processes a video from the {@link inputFileName} (from the {@link rawVideoBucketName}) and saves it to the {@link outputFileName}. After the video has been processed, it is uploaded to the {@link processedVideoBucketName} bucket.
-
- * @param req - The request object.
- * @param res - The response object.
- * @returns A response with a status code and a message.
+ * @description Processes a video from the {@link inputFilePath} and saves it to the {@link outputFilePath}.
  */
 export const processVideo = async (req: Request, res: Response) => {
-  const { inputFileName, outputFileName } = req.body;
+  const { inputFilePath, outputFilePath } = req.body;
 
-  if (!inputFileName || !outputFileName) {
+  if (!inputFilePath || !outputFilePath) {
     return res.status(400).json({
-      message:
-        "BAD REQUEST: Please provide a valid inputFileName and outputFileName"
+      message: "ERROR: Please provide a valid inputFilePath and outputFilePath"
     });
   }
 
-  convertVideo(inputFileName, outputFileName)
-    .then(() => {
-      return res.status(200).json({
-        message: "Video processed successfully"
-      });
-    })
-    .catch(err => {
-      return res.status(500).json({
-        message: `INTERNAL SERVER ERROR: ${err.message}`
-      });
+  try {
+    await convertVideoWithFFmpeg(
+      inputFilePath,
+      outputFilePath,
+      DEFAULT_VIDEO_RESOLUTION
+    );
+
+    return res.status(200).json({
+      message: `Video processed successfully: ${outputFilePath}`
     });
+  } catch (err) {
+    console.error(err);
+
+    return res.status(500).json({
+      message: `ERROR: ${(err as Error).message}`
+    });
+  }
 };
