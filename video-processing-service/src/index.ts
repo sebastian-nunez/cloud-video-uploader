ccad0dcc6646fff137938b9c127d40519790a900
sebastian-nunez <02snunez@gmail.com>
2023-11-20 09:59:24 -0500

    Update server port configuration

--- video-processing-service/src/index.ts
+++ video-processing-service/src/index.ts
@@ -15,7 +15,7 @@ app.use(express.json());
 // routes
 app.use("/process-video", processVideoRouter);
 
-const PORT = process.env.SERVER_PORT || 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Video processing service listening at http://localhost:${PORT}`);
 });
