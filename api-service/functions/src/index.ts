cb3e54334df85a1aa84b3461c9ea5d99555c8a0d
sebastian-nunez <02snunez@gmail.com>
2023-11-22 00:11:39 -0500

    added a generate Signed url video upload cloud function

--- api-service/functions/src/index.ts
+++ api-service/functions/src/index.ts
@@ -7,11 +7,12 @@
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
 
-// prettier-ignore
-import {initializeApp} from "firebase-admin/app";
-import {Firestore} from "firebase-admin/firestore";
+import { Storage } from "@google-cloud/storage";
+import { initializeApp } from "firebase-admin/app";
+import { Firestore } from "firebase-admin/firestore";
 import * as functions from "firebase-functions";
-import {logger} from "firebase-functions";
+import { logger } from "firebase-functions";
+import { onCall } from "firebase-functions/v2/https";
 
 // Start writing functions
 // https://firebase.googlçe.com/docs/functions/typescript
@@ -22,17 +23,48 @@ import {logger} from "firebase-functions";
 //   res.status(200).json({ message: "Hello from Firebase!" });
 // });
 
+const rawVideoBucketName = "cloud-video-uploader-raw-videos";
+
 initializeApp();
 const firestore = new Firestore();
+const storage = new Storage();
 
-export const createUser = functions.auth.user().onCreate((user) => {
+export const createUser = functions.auth.user().onCreate(user => {
   const userInfo = {
     userId: user.uid,
     email: user.email,
     name: user.displayName,
-    photoUrl: user.photoURL,
+    photoUrl: user.photoURL
   };
 
   firestore.collection("users").doc(user.uid).set(userInfo);
   logger.info(`User created: ${JSON.stringify(userInfo)}`);
 });
+
+export const generateSignedUploadUrl = onCall(
+  { maxInstances: 1 },
+  async request => {
+    // check if user is authenticated
+    if (!request.auth) {
+      throw new functions.https.HttpsError(
+        "unauthenticated",
+        "Only authenticated users can upload videos"
+      );
+    }
+
+    const { auth, data } = request;
+    const bucket = storage.bucket(rawVideoBucketName);
+
+    // generate a unique file name
+    const fileName = `${auth.uid}-${Date.now()}.${data?.fileExtension}`;
+
+    const [url] = await bucket.file(fileName).getSignedUrl({
+      version: "v4",
+      action: "write",
+      expires: Date.now() + 15 * 60 * 1000 // 15 minutes
+    });
+
+    logger.info(`Generated signed URL for ${fileName}`);
+    return { url, fileName };
+  }
+);
