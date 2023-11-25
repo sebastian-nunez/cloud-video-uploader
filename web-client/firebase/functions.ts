ae30941f85f034f67a20bffcd4dff9d8943c6383
sebastian-nunez <02snunez@gmail.com>
2023-11-25 06:28:25 -0500

    added the uploadVideo cloud function to frontend

---
+++ web-client/firebase/functions.ts
@@ -0,0 +1,31 @@
+import { getFunctions, httpsCallable } from "firebase/functions";
+
+const functions = getFunctions();
+
+const generateUploadUrlRawVideo = httpsCallable(
+  functions,
+  "generateSignedUploadUrlForRawVideos"
+);
+
+export const uploadVideo = async (file: File) => {
+  // generate a signed URL
+  const response: any = await generateUploadUrlRawVideo({
+    fileExtension: file.name.split(".").pop()
+  });
+
+  // upload the file to the signed URL
+  const res = await fetch(response?.data?.url, {
+    method: "PUT",
+    body: file,
+    headers: {
+      "Content-Type": file.type
+    }
+  });
+
+  if (res.ok) {
+    console.log(`Uploaded file '${file.name}' via a signed URL`);
+  }
+
+  // return the file name
+  return response?.data?.fileName;
+};
