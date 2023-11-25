f0ff61d1d2e60373ed8f8cd0c642812707c91c72
sebastian-nunez <02snunez@gmail.com>
2023-11-25 09:11:13 -0500

    Add Video type definition to types.ts

--- video-processing-service/src/utils/types.ts
+++ video-processing-service/src/utils/types.ts
@@ -2,3 +2,12 @@
  * Reference: https://github.com/fluent-ffmpeg/node-fluent-ffmpeg#video-frame-size-options
  */
 export type FfmpegVideoResolution = "?x360" | "?480" | "?x720" | "?x1080";
+
+export type Video = {
+  id?: string;
+  uid?: string;
+  filename?: string;
+  status?: "processing" | "processed";
+  title?: string;
+  description?: string;
+};
