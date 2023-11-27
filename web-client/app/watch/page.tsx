0d3a5e7fe5cce787705902c130a7b899a19da354
sebastian-nunez <02snunez@gmail.com>
2023-11-27 15:02:42 -0500

    added the Watch page

--- web-client/app/watch/page.tsx
+++ web-client/app/watch/page.tsx
@@ -1,7 +1,23 @@
-const Watch: React.FC = () => {
+"use client";
+
+import { useSearchParams } from "next/navigation";
+
+const Watch = () => {
+  const videoPrefix =
+    "https://storage.googleapis.com/cloud-video-uploader-processed-videos";
+  const videoName = useSearchParams().get("v");
+
   return (
-    <div>
-      <h1>Watch</h1>
+    <div className="flex flex-col gap-4">
+      <div className="w-2/3">
+        <video
+          controls
+          src={`${videoPrefix}/${videoName}`}
+          className="w-full h-full"
+        />
+      </div>
+
+      <div></div>
     </div>
   );
 };
